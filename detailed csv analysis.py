18b3c1aff50ca549d91a8bb22e0eef525ada0b64
Hyrum-Taylor <82358970+Hyrum-Taylor@users.noreply.github.com>
2025-03-06 12:11:23 -0700

    csv graphing added new graph to imitate one that comes from the main program

--- detailed csv analysis.py
+++ detailed csv analysis.py
@@ -3,27 +3,22 @@ import matplotlib.pyplot as plt
 import csv
 import os
 
-# # --- Save the error data! I want to make figures from this later! ---
-# csv_name = datetime.datetime.now().strftime('%Y-%m-%d %H-%M-%S') + ' 2_cam_stabilization.csv'
-# with open(os.path.join(os.getcwd(),'CSV',csv_name), 'w', newline="") as f:
-#     writer = csv.writer(f)
-#     names = [["time_steps1","y_err1","x_err1","tot_err1","time_steps2","y_err2","x_err2","tot_err2"]]
-
-#     writer.writerows(names)
-#     writer.writerows(np.transpose([time_steps_1,y_vals1,x_vals1,tot_err1, time_steps_2,y_vals2,x_vals2,tot_err2]))
-
 csv_name = "mar5 1 fighting PI.csv"
 csv_file_path = os.path.join(os.getcwd(),'CSV',csv_name)
+arr = []
 with open(csv_file_path, 'r') as f:
     data = csv.reader(f)
     next(data)
-    arr = []
     for line in data:
         arr.append(line)
     arr = np.array(arr).astype(float)
-    print(arr.shape)
 
-    #  [["time_steps1","y_err1","x_err1","tot_err1","time_steps2","y_err2","x_err2","tot_err2"]]
+
+# --- Now that I have the data pulled out of the file, I can make graphs with it! (first index is time, second is data type)
+#           ["time_steps1","y_err1","x_err1","tot_err1","time_steps2","y_err2","x_err2","tot_err2"]
+
+
+if True:  # This graph shows the x and y error for both cameras over time
 
     fig, ax = plt.subplots(2,1, figsize=(8,5))
     plt.suptitle(csv_name, fontweight="bold")
@@ -34,7 +29,7 @@ with open(csv_file_path, 'r') as f:
     ax[0].set_title("camera 1")
     ax[0].set_xlabel("time")
     ax[0].set_ylabel("pixels")
-    ax[0].legend(loc="upper left")
+    ax[0].legend(loc="upper right", fancybox=True, framealpha=1)
 
     ax[1].plot(arr[:,4]-arr[0,4], arr[:,5],'.-b', label="y err")
     ax[1].plot(arr[:,4]-arr[0,4], arr[:,6],'.-r', label="x err")
@@ -42,7 +37,19 @@ with open(csv_file_path, 'r') as f:
     ax[1].set_title("camera 2")
     ax[1].set_xlabel("time")
     ax[1].set_ylabel("pixels")
-    ax[1].legend(loc="upper left")
+    ax[1].legend(loc="upper right", fancybox=True, framealpha=1)
 
     plt.tight_layout()
+    plt.show()
+
+
+if False: # This graph recreates the one made in the main program, but better labeling and shape
+
+    plt.figure(figsize=(8,4))
+    plt.plot(arr[:,0]-arr[0,0], arr[:,3], '-.b', label="Cam 1")
+    plt.plot(arr[:,4]-arr[0,4], arr[:,7], '-.r', label="Cam 2")
+
+    time_elapsed = max(arr[-1,0],arr[-1,4]) - max(arr[0,0],arr[0,4])
+    plt.title("Total error for cameras 1,2 over time={:.2f} s".format(time_elapsed), fontweight="bold")
+    plt.legend(loc="upper right", fancybox=True, framealpha=1)
     plt.show()
